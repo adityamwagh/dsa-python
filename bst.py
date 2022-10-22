9b49fbbbaf3395ab7e9ae58de70263d5a5fb2da8
Aditya Wagh <adityamwagh@outlook.com>
2022-10-22 17:25:41 -0400

    Create bst.py
    binary search tree implementation

---
+++ bst.py
@@ -0,0 +1,15 @@
+"""
+Binary Search Tree some common methods
+"""
+
+class TreeNode(val):
+  
+  def __init__(self, val):
+    
+    self.val = val
+    self.left = None
+    self.right = None
+    
+class BinarySearchTree():
+  
+  pass
